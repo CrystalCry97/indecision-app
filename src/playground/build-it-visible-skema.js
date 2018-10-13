b5b59c4a50c6feb01a41e8c02153b56be4509320
amalsyahmi97 <amalsyahmi97@student.usm.my>
2018-10-13 13:59:11 +0800

    implement class in built-it-visible-skema

--- src/playground/build-it-visible-skema.js
+++ src/playground/build-it-visible-skema.js
@@ -1,29 +1,33 @@
-let visibility= false;
+class VisibilityToggle extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return{
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility? 'Hide details' : 'Show details'}</button>
+                {
+                    this.state.visibility && (
+                        <div>
+                            <p>Hey. These are some details you can now see!!!</p>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
 
-const toggleVisibility = () =>{
-    visibility = !visibility;
-    render();
-};
-
-
-const appRoot= document.getElementById('app'); //set destination to id 'app' at index.html
-
-const render = () =>{
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>{visibility? 'Hide details' : 'Show details'}</button>
-            {
-                visibility && (
-                    <div>
-                        <p>Hey. These are some details you can now see!!!</p>
-                    </div>
-                )
-            }
-        </div>
-    );
-
-    ReactDOM.render(template, appRoot); //render anything inside template at 'app' index.html
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />,document.getElementById('app'));
