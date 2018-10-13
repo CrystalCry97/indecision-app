2eabc3b2d5b2ad31732733b25bc1c78483a7b660
amalsyahmi97 <amalsyahmi97@student.usm.my>
2018-10-14 05:58:50 +0800

    simplify some codes & removing individual option

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,6 +19,7 @@ var IndecisionApp = function (_React$Component) {
         _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
         _this.handlePick = _this.handlePick.bind(_this);
         _this.handleAddOption = _this.handleAddOption.bind(_this);
+        _this.handleDeleteOption = _this.handleDeleteOption.bind(_this);
         _this.state = {
             options: props.options
         };
@@ -29,9 +30,16 @@ var IndecisionApp = function (_React$Component) {
         key: 'handleDeleteOptions',
         value: function handleDeleteOptions() {
             this.setState(function () {
-                return {
-                    options: []
-                };
+                return { options: [] };
+            });
+        }
+    }, {
+        key: 'handleDeleteOption',
+        value: function handleDeleteOption(optiontoRemove) {
+            this.setState(function (prevState) {
+                return { options: prevState.options.filter(function (option) {
+                        return optiontoRemove != option;
+                    }) };
             });
         }
     }, {
@@ -55,9 +63,7 @@ var IndecisionApp = function (_React$Component) {
             }
 
             this.setState(function (prevState) {
-                return {
-                    options: prevState.options.concat(option)
-                };
+                return { options: prevState.options.concat(option) };
             });
         }
     }, {
@@ -74,7 +80,8 @@ var IndecisionApp = function (_React$Component) {
                     handlePick: this.handlePick }),
                 React.createElement(Options, {
                     options: this.state.options,
-                    handleDeleteOptions: this.handleDeleteOptions }),
+                    handleDeleteOptions: this.handleDeleteOptions,
+                    handleDeleteOption: this.handleDeleteOption }),
                 React.createElement(AddOption, {
                     handleAddOption: this.handleAddOption })
             );
@@ -137,7 +144,10 @@ var Options = function Options(props) {
             'Remove All'
         ),
         props.options.map(function (option) {
-            return React.createElement(Option, { key: option, optionText: option });
+            return React.createElement(Option, {
+                key: option,
+                optionText: option,
+                handleDeleteOption: props.handleDeleteOption });
         })
     );
 };
@@ -146,7 +156,15 @@ var Option = function Option(props) {
     return React.createElement(
         'div',
         null,
-        props.optionText
+        props.optionText,
+        React.createElement(
+            'button',
+            {
+                onClick: function onClick(e) {
+                    props.handleDeleteOption(props.optionText);
+                } },
+            'remove'
+        )
     );
 };
 
--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ class IndecisionApp extends React.Component{
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption= this.handleAddOption.bind(this);
+        this.handleDeleteOption= this.handleDeleteOption.bind(this);
         this.state ={
             options: props.options
         };
     }
     handleDeleteOptions(){
-        this.setState(() => {
-            return{
-                options: []
-            };
-        });
+        this.setState(() => ({ options: [] }));
+    }
+    handleDeleteOption(optiontoRemove){
+        this.setState((prevState)=>({options: prevState.options.filter((option) => optiontoRemove != option)}));
     }
     handlePick(){
         this.setState(()=>{
@@ -29,11 +29,7 @@ class IndecisionApp extends React.Component{
             return ('This option already exists')
         }
 
-        this.setState((prevState) =>{
-            return {
-                options: prevState.options.concat(option)
-            };
-        });
+        this.setState((prevState) =>({ options: prevState.options.concat(option) }));
     }
     render(){
         const subtitle = 'Put your life in the hands of a computer';
@@ -46,7 +42,8 @@ class IndecisionApp extends React.Component{
                     handlePick={this.handlePick}/>
                 <Options 
                     options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}/>
+                    handleDeleteOptions={this.handleDeleteOptions}
+                    handleDeleteOption={this.handleDeleteOption}/>
                 <AddOption
                     handleAddOption={this.handleAddOption}/>
             </div>
@@ -87,7 +84,12 @@ const Options = (props) => {
                 <h3>Number of options: {props.options.length}</h3>
                 <button onClick={props.handleDeleteOptions}>Remove All</button>
                 {
-                    props.options.map((option)=><Option key={option} optionText={option}/>)
+                    props.options.map((option)=>(
+                        <Option 
+                            key={option} 
+                            optionText={option}
+                            handleDeleteOption={props.handleDeleteOption}/>
+                    ))
                 }
             </div>
         );
@@ -97,6 +99,12 @@ const Option = (props) => {
         return(
             <div>
                 {props.optionText}
+                <button 
+                    onClick={(e) =>{
+                        props.handleDeleteOption(props.optionText);
+                    }}>
+                    remove
+                </button>
             </div>
         );
 }
@@ -117,9 +125,7 @@ class AddOption extends React.Component {
         
         const error= this.props.handleAddOption(option);
 
-        this.setState(() =>{
-            return {error};
-        }); 
+        this.setState(() =>({ error })); 
     }
     render(){
         return (
